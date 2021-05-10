5dea15b99edfc97d340ce66271fb1d3a2ec192f5
shaoyingzhu <shaoyingzhu@basestonedata.com>
2021-05-10 14:59:23 +0800

    寻找旋转排序数组中的最小值 II

--- src/main/java/com/syz/algorithm/algorithm/linkedList/easy/Test1.java
+++ src/main/java/com/syz/algorithm/algorithm/linkedList/easy/Test1.java
@@ -6,6 +6,56 @@ import com.syz.algorithm.algorithm.linkedList.easy.test1.ListNode;
 //合并两个有序链表
 public class Test1 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+/*        if(l1 ==null && l2==null){
+           return null;
+        }else if(l1 ==null ){
+            return l2;
+        }else if(l2 ==null){
+            return l1;
+        }else {
+            if(l1.val<l2.val){
+                l1.next =    mergeTwoLists(l1.next,l2);
+                return l1;
+            }else {
+                l2.next =   mergeTwoLists(l2.next,l1);
+                return l2;
+            }
+        }*/
+
+        ListNode listNode=new ListNode(-1);
+        if(l1==null){
+            return l2;
+        }else if(l2==null){
+            return l1;
+        }
+
+        while (l1==null || l2==null){
+            if(l1.val<l2.val){
+                listNode.next=l1;
+                l1=l1.next;
+            }else {
+                listNode.next=l2;
+                l2=l2.next;
+            }
+            listNode= listNode.next;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       //  https://leetcode-cn.com/problems/merge-two-sorted-lists/solution/he-bing-liang-ge-you-xu-lian-biao-by-leetcode-solu/
 /*
         我们直接将以上递归过程建模，同时需要考虑边界情况。
@@ -54,6 +104,19 @@ public class Test1 {
         prev.next = l1 == null ? l2 : l1;
 
         return prehead.next;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
